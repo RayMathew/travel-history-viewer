4b27c6a2c9d9f44294c657f1ecf1445fe2167941
Ray Mathew <ray10mathew@gmail.com>
2024-11-05 14:06:06 -0800

    added sample google map and custom svg pin for testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,129 @@
-import Image from "next/image";
+// import Image from "next/image";
+"use client";
+
+import React from 'react';
+import { AdvancedMarker, APIProvider, Map } from '@vis.gl/react-google-maps';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+  if (!apiKey) {
+    throw new Error('API key is not defined. Please set the NEXT_PUBLIC_API_KEY environment variable.');
+  }
+  const position = { lat: 53.54992, lng: 10.00678 };
+
+
+  const PinSvg = () => <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none">
+    <rect width="56" height="56" rx="28" fill="#7837FF"></rect><path d="M46.0675 22.1319L44.0601 22.7843" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M11.9402 33.2201L9.93262 33.8723" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M27.9999 47.0046V44.8933" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M27.9999 9V11.1113" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M39.1583 43.3597L37.9186 41.6532" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M16.8419 12.6442L18.0816 14.3506" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M9.93262 22.1319L11.9402 22.7843" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M46.0676 33.8724L44.0601 33.2201" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M39.1583 12.6442L37.9186 14.3506" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M16.8419 43.3597L18.0816 41.6532" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M28 39L26.8725 37.9904C24.9292 36.226 23.325 34.7026 22.06 33.4202C20.795 32.1378 19.7867 30.9918 19.035 29.9823C18.2833 28.9727 17.7562 28.0587 17.4537 27.2401C17.1512 26.4216 17 25.5939 17 24.7572C17 23.1201 17.5546 21.7513 18.6638 20.6508C19.7729 19.5502 21.1433 19 22.775 19C23.82 19 24.7871 19.2456 25.6762 19.7367C26.5654 20.2278 27.34 20.9372 28 21.8649C28.77 20.8827 29.5858 20.1596 30.4475 19.6958C31.3092 19.2319 32.235 19 33.225 19C34.8567 19 36.2271 19.5502 37.3362 20.6508C38.4454 21.7513 39 23.1201 39 24.7572C39 25.5939 38.8488 26.4216 38.5463 27.2401C38.2438 28.0587 37.7167 28.9727 36.965 29.9823C36.2133 30.9918 35.205 32.1378 33.94 33.4202C32.675 34.7026 31.0708 36.226 29.1275 37.9904L28 39Z" fill="#FF7878"></path>
+  </svg>
+
+
+  return (
+    <APIProvider apiKey={apiKey}>
+      <div className="w-1/2 h-dvh">
+        <Map defaultCenter={position} defaultZoom={10} mapId="DEMO_MAP_ID">
+          <AdvancedMarker position={position}>
+            <PinSvg />
+          </AdvancedMarker>
+        </Map>
+      </div>
+    </APIProvider>
+  )
+  // return (
+  //   <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+  //     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+  //       <Image
+  //         className="dark:invert"
+  //         src="/next.svg"
+  //         alt="Next.js logo"
+  //         width={180}
+  //         height={38}
+  //         priority
+  //       />
+  //       <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+  //         <li className="mb-2">
+  //           Get started by editing{" "}
+  //           <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+  //             app/page.tsx
+  //           </code>
+  //           .
+  //         </li>
+  //         <li>Save and see your changes instantly.</li>
+  //       </ol>
+
+  //       <div className="flex gap-4 items-center flex-col sm:flex-row">
+  //         <a
+  //           className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+  //           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+  //           target="_blank"
+  //           rel="noopener noreferrer"
+  //         >
+  //           <Image
+  //             className="dark:invert"
+  //             src="/vercel.svg"
+  //             alt="Vercel logomark"
+  //             width={20}
+  //             height={20}
+  //           />
+  //           Deploy now
+  //         </a>
+  //         <a
+  //           className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+  //           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+  //           target="_blank"
+  //           rel="noopener noreferrer"
+  //         >
+  //           Read our docs
+  //         </a>
+  //       </div>
+  //     </main>
+  //     <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+  //       <a
+  //         className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+  //         href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         <Image
+  //           aria-hidden
+  //           src="/file.svg"
+  //           alt="File icon"
+  //           width={16}
+  //           height={16}
+  //         />
+  //         Learn
+  //       </a>
+  //       <a
+  //         className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+  //         href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         <Image
+  //           aria-hidden
+  //           src="/window.svg"
+  //           alt="Window icon"
+  //           width={16}
+  //           height={16}
+  //         />
+  //         Examples
+  //       </a>
+  //       <a
+  //         className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+  //         href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         <Image
+  //           aria-hidden
+  //           src="/globe.svg"
+  //           alt="Globe icon"
+  //           width={16}
+  //           height={16}
+  //         />
+  //         Go to nextjs.org →
+  //       </a>
+  //     </footer>
+  //   </div>
+  // );
 }
