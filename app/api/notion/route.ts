98d415513f52c84365c8660bc62d9a2bf1581480
Ray Mathew <ray10mathew@gmail.com>
2024-11-08 17:55:21 -0800

    fetch data from Notion - Outdoors DB

--- app/api/notion/route.ts
+++ app/api/notion/route.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from "next/server";
-import { fetchNotionData } from "@/lib/notion";
+import { fetchOutdoorsDBData } from "@/lib/notion";
 
 export async function GET() {
   try {
-    const data = await fetchNotionData();
+    const data = await fetchOutdoorsDBData();
     return NextResponse.json(data);
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
--- lib/notion.ts
+++ lib/notion.ts
@@ -14,3 +14,71 @@ export const fetchNotionData = async () => {
 
   return listUsersResponse;
 };
+
+export const fetchOutdoorsDBData = async () => {
+  const outdoorsData = [];
+  //   console.time("outdoors data");
+  const dbData = await getNotionClient().databases.query({
+    database_id: process.env.NOTION_OUTDOORSDB_KEY!,
+  });
+
+  //   console.log(dbData);
+  try {
+    dbData.results.forEach((page) => {
+      const { properties } = page;
+      const type = getActivityType(properties["Tags"].multi_select);
+      const { lat, lng } = getCoordinates(properties["Coordinates"]);
+      const date = properties["Date"].date.start;
+      const distance = properties["Distance (miles)"].number;
+      const doneBy = getPeople(properties["Done by"].people);
+      const elevation = properties["Elevation (ft)"].number;
+      const name = properties["Name"].title[0].plain_text;
+      const allTrailsLink = properties["All Trails"].url;
+      const googlePhotosLink = properties["Google Photos Album"].url;
+      const instagramLink = properties["Instagram"].url;
+
+      outdoorsData.push({
+        type,
+        coordinates: { lat, lng },
+        date,
+        distance,
+        doneBy,
+        elevation,
+        name,
+        allTrailsLink,
+        googlePhotosLink,
+        instagramLink,
+      });
+    });
+
+    // console.timeEnd("outdoors data");
+    // console.log(outdoorsData.toString());
+  } catch (error) {
+    console.log("error", error);
+  }
+  return outdoorsData;
+};
+
+const getActivityType = (tagArray): string => {
+  const tag = tagArray.find(
+    (tagObject) => tagObject.name == "Hiking" || tagObject.name == "Biking"
+  );
+  return tag.name;
+};
+
+const getCoordinates = (coordinatesObj) => {
+  const lat = parseFloat(coordinatesObj.rich_text[0].plain_text.split(",")[0]);
+  const lng = parseFloat(
+    coordinatesObj.rich_text[0].plain_text.split(",")[1].trim()
+  );
+
+  return { lat, lng };
+};
+
+const getPeople = (peopleObjArray) => {
+  const people: string[] = [];
+  //   console.log("peopleObjArray", peopleObjArray);
+  peopleObjArray.forEach((peopleObj) => people.push(peopleObj.name));
+
+  return people;
+};
