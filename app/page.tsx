04fa740667365dc63a6f89a2f4d33d43bc65b980
Ray Mathew <ray10mathew@gmail.com>
2024-11-09 20:36:39 -0800

    introduce filter function and show only current year data on website load

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,15 @@
-// import Image from "next/image";
 "use client";
 
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image'
 import { AdvancedMarker, APIProvider, Map, useMap } from '@vis.gl/react-google-maps';
-import { RAY, NAMRATA, HIKING, BIKING, TRAVEL } from '../lib/constants';
+import { getActivityImgSrc, applyFiltersToMap } from '@/lib/maphelper';
 
 export default function Home() {
   const [notionData, setNotionData] = useState(null);
+  const [displayData, setDisplayData] = useState(null);
   const [loading, setLoading] = useState(true);
-  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
-
-  if (!apiKey) {
-    throw new Error('API key is not defined. Please set the NEXT_PUBLIC_API_KEY environment variable.');
-  }
+  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!;
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,7 +17,10 @@ export default function Home() {
         const response = await fetch('/api/notion');
         const data = await response.json();
         setNotionData(data);
-        // console.log(data);
+
+        const filteredData = applyFiltersToMap(true, data);
+        setDisplayData(filteredData);
+
       } catch (error) {
         console.error('Error fetching data from Notion:', error);
       } finally {
@@ -31,12 +30,6 @@ export default function Home() {
 
     fetchData();
   }, []);
-
-  useEffect(() => {
-    if (!notionData) return;
-
-
-  }, [notionData]);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -49,23 +42,13 @@ export default function Home() {
   const CustomMap = () => {
     const map = useMap();
 
-    const getActivityImgSrc = activityData => {
-      if (activityData.type == TRAVEL) return '/airplane.png';
-
-      if (activityData.doneBy.includes(RAY)) {
-        if (activityData.type == HIKING) return '/malewalk.png';
-        return '/malebicycle.png';
-      }
-
-      else if (activityData.type == HIKING) return '/femalewalk.png';
-      return '/femalebicycle.png';
-    }
-
     useEffect(() => {
-      if (!map || !notionData) return;
-      let bounds = new window.google.maps.LatLngBounds();
+      if (!map || !notionData || !displayData) return;
+      const bounds = new window.google.maps.LatLngBounds();
 
-      const { outdoorsData, travelData } = notionData;
+      const { outdoorsData, travelData } = displayData;
+
+
       for (var i = 0; i < outdoorsData.length; i++) {
         bounds.extend(new window.google.maps.LatLng(outdoorsData[i].coordinates.lat, outdoorsData[i].coordinates.lng));
       }
@@ -79,8 +62,9 @@ export default function Home() {
     }, [map]);
 
     return (
-      <Map mapId="DEMO" mapTypeControl={false} streetViewControl={false}>
-        {notionData.outdoorsData.map((activityData, index) => {
+      <Map mapId="DEMO" defaultZoom={2} defaultCenter={{ lat: 23.468, lng: 10.872 }} mapTypeControl={false} streetViewControl={false}>
+        {displayData.outdoorsData.map((activityData, index) => {
+          // collisionBehavior={CollisionBehavior.OPTIONAL_AND_HIDES_LOWER_PRIORITY}
           return (<AdvancedMarker key={index} position={{ lat: activityData.coordinates.lat, lng: activityData.coordinates.lng }}>
             <Image
               src={getActivityImgSrc(activityData)}
@@ -90,8 +74,9 @@ export default function Home() {
             />
           </AdvancedMarker>)
         })}
-        {notionData.travelData.map((activityData) => {
+        {displayData.travelData.map((activityData) => {
           return activityData.coordinatesArray.map((coordinatesObj, index) => {
+            // collisionBehavior={CollisionBehavior.OPTIONAL_AND_HIDES_LOWER_PRIORITY}
             return (<AdvancedMarker key={index} position={{ lat: coordinatesObj.lat, lng: coordinatesObj.lng }}>
               <Image
                 src={getActivityImgSrc(activityData)}
@@ -119,6 +104,7 @@ export default function Home() {
               width={44}
               height={44}
               alt="Go to stats page"
+              priority={false}
             />
           </div>
           <div className='flex-1'></div>
@@ -131,101 +117,4 @@ export default function Home() {
       </div>
     </div>
   )
-  // return (
-  //   <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-  //     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-  //       <Image
-  //         className="dark:invert"
-  //         src="/next.svg"
-  //         alt="Next.js logo"
-  //         width={180}
-  //         height={38}
-  //         priority
-  //       />
-  //       <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-  //         <li className="mb-2">
-  //           Get started by editing{" "}
-  //           <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-  //             app/page.tsx
-  //           </code>
-  //           .
-  //         </li>
-  //         <li>Save and see your changes instantly.</li>
-  //       </ol>
-
-  //       <div className="flex gap-4 items-center flex-col sm:flex-row">
-  //         <a
-  //           className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-  //           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-  //           target="_blank"
-  //           rel="noopener noreferrer"
-  //         >
-  //           <Image
-  //             className="dark:invert"
-  //             src="/vercel.svg"
-  //             alt="Vercel logomark"
-  //             width={20}
-  //             height={20}
-  //           />
-  //           Deploy now
-  //         </a>
-  //         <a
-  //           className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-  //           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-  //           target="_blank"
-  //           rel="noopener noreferrer"
-  //         >
-  //           Read our docs
-  //         </a>
-  //       </div>
-  //     </main>
-  //     <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-  //       <a
-  //         className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-  //         href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         <Image
-  //           aria-hidden
-  //           src="/file.svg"
-  //           alt="File icon"
-  //           width={16}
-  //           height={16}
-  //         />
-  //         Learn
-  //       </a>
-  //       <a
-  //         className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-  //         href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         <Image
-  //           aria-hidden
-  //           src="/window.svg"
-  //           alt="Window icon"
-  //           width={16}
-  //           height={16}
-  //         />
-  //         Examples
-  //       </a>
-  //       <a
-  //         className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-  //         href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         <Image
-  //           aria-hidden
-  //           src="/globe.svg"
-  //           alt="Globe icon"
-  //           width={16}
-  //           height={16}
-  //         />
-  //         Go to nextjs.org →
-  //       </a>
-  //     </footer>
-  //   </div>
-  // );
 }
---
+++ lib/maphelper.ts
@@ -0,0 +1,37 @@
+import { RAY, NAMRATA, HIKING, BIKING, TRAVEL } from "@/lib/constants";
+
+const currentYear = new Date().getFullYear();
+
+export const getActivityImgSrc = (activityData) => {
+  if (activityData.type == TRAVEL) return "/airplane.png";
+
+  if (activityData.doneBy.includes(RAY)) {
+    if (activityData.type == HIKING) return "/malewalk.png";
+    return "/malebicycle.png";
+  } else if (activityData.type == HIKING) return "/femalewalk.png";
+  return "/femalebicycle.png";
+};
+
+export const applyFiltersToMap = (
+  initialLoad,
+  notionData,
+  filter = { year: currentYear }
+) => {
+  const filterYear = filter.year;
+  const outdoorsData = notionData.outdoorsData.filter((activityData) => {
+    return new Date(activityData.date).getFullYear() === filterYear;
+  });
+
+  const travelData = notionData.outdoorsData.filter((activityData) => {
+    if (!activityData.startDate) return false;
+
+    return new Date(activityData.startDate).getFullYear() === filterYear;
+  });
+
+  // recursive function that runs the initial load filter again with the previous year if current year data is empty
+  if (outdoorsData.length === 0 && travelData.length === 0 && initialLoad) {
+    return applyFiltersToMap(true, notionData, { year: filterYear - 1 });
+  }
+
+  return { outdoorsData, travelData };
+};
