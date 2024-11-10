a2b1466b5178ca3fe6d83174b080bff9b0fe3d7b
Ray Mathew <ray10mathew@gmail.com>
2024-11-09 16:32:55 -0800

    add LatLngBounds for auto-centering behaviour

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image'
-import { AdvancedMarker, APIProvider, Map } from '@vis.gl/react-google-maps';
+import { AdvancedMarker, APIProvider, Map, useMap } from '@vis.gl/react-google-maps';
 import { RAY, NAMRATA, HIKING, BIKING, TRAVEL } from '../lib/constants';
 
 export default function Home() {
@@ -46,23 +46,65 @@ export default function Home() {
     return <div>Error fetching Notion data</div>;
   }
 
-  const getActivityImgSrc = activityData => {
-    if (activityData.type == TRAVEL) return '/airplane.png';
+  const CustomMap = () => {
+    const map = useMap();
 
-    if (activityData.doneBy.includes(RAY)) {
-      if (activityData.type == HIKING) return '/malewalk.png';
-      return '/malebicycle.png';
+    const getActivityImgSrc = activityData => {
+      if (activityData.type == TRAVEL) return '/airplane.png';
+
+      if (activityData.doneBy.includes(RAY)) {
+        if (activityData.type == HIKING) return '/malewalk.png';
+        return '/malebicycle.png';
+      }
+
+      else if (activityData.type == HIKING) return '/femalewalk.png';
+      return '/femalebicycle.png';
     }
 
-    else if (activityData.type == HIKING) return '/femalewalk.png';
-    return '/femalebicycle.png';
-  }
-  // const position = { lat: 53.54992, lng: 10.00678 };
+    useEffect(() => {
+      if (!map || !notionData) return;
+      let bounds = new window.google.maps.LatLngBounds();
 
+      const { outdoorsData, travelData } = notionData;
+      for (var i = 0; i < outdoorsData.length; i++) {
+        bounds.extend(new window.google.maps.LatLng(outdoorsData[i].coordinates.lat, outdoorsData[i].coordinates.lng));
+      }
+      for (var i = 0; i < travelData.length; i++) {
+        for (var k = 0; k < travelData[i].coordinatesArray.length; k++) {
+          bounds.extend(new window.google.maps.LatLng(travelData[i].coordinatesArray[k].lat, travelData[i].coordinatesArray[k].lng));
+        }
+      }
 
-  // const PinSvg = () => <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none">
-  //   <rect width="56" height="56" rx="28" fill="#7837FF"></rect><path d="M46.0675 22.1319L44.0601 22.7843" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M11.9402 33.2201L9.93262 33.8723" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M27.9999 47.0046V44.8933" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M27.9999 9V11.1113" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M39.1583 43.3597L37.9186 41.6532" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M16.8419 12.6442L18.0816 14.3506" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M9.93262 22.1319L11.9402 22.7843" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M46.0676 33.8724L44.0601 33.2201" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M39.1583 12.6442L37.9186 14.3506" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M16.8419 43.3597L18.0816 41.6532" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M28 39L26.8725 37.9904C24.9292 36.226 23.325 34.7026 22.06 33.4202C20.795 32.1378 19.7867 30.9918 19.035 29.9823C18.2833 28.9727 17.7562 28.0587 17.4537 27.2401C17.1512 26.4216 17 25.5939 17 24.7572C17 23.1201 17.5546 21.7513 18.6638 20.6508C19.7729 19.5502 21.1433 19 22.775 19C23.82 19 24.7871 19.2456 25.6762 19.7367C26.5654 20.2278 27.34 20.9372 28 21.8649C28.77 20.8827 29.5858 20.1596 30.4475 19.6958C31.3092 19.2319 32.235 19 33.225 19C34.8567 19 36.2271 19.5502 37.3362 20.6508C38.4454 21.7513 39 23.1201 39 24.7572C39 25.5939 38.8488 26.4216 38.5463 27.2401C38.2438 28.0587 37.7167 28.9727 36.965 29.9823C36.2133 30.9918 35.205 32.1378 33.94 33.4202C32.675 34.7026 31.0708 36.226 29.1275 37.9904L28 39Z" fill="#FF7878"></path>
-  // </svg>
+      map.fitBounds(bounds);
+    }, [map]);
+
+    return (
+      <Map mapId="DEMO" mapTypeControl={false} streetViewControl={false}>
+        {notionData.outdoorsData.map((activityData, index) => {
+          return (<AdvancedMarker key={index} position={{ lat: activityData.coordinates.lat, lng: activityData.coordinates.lng }}>
+            <Image
+              src={getActivityImgSrc(activityData)}
+              width={36}
+              height={36}
+              alt={activityData.type}
+            />
+          </AdvancedMarker>)
+        })}
+        {notionData.travelData.map((activityData) => {
+          return activityData.coordinatesArray.map((coordinatesObj, index) => {
+            return (<AdvancedMarker key={index} position={{ lat: coordinatesObj.lat, lng: coordinatesObj.lng }}>
+              <Image
+                src={getActivityImgSrc(activityData)}
+                width={36}
+                height={36}
+                alt={activityData.type}
+              />
+            </AdvancedMarker>)
+          })
+        })}
+      </Map>
+    );
+  };
 
 
   return (
@@ -84,32 +126,7 @@ export default function Home() {
       </div>
       <div className="md:w-2/3 2xl:flex-1 h-dvh">
         <APIProvider apiKey={apiKey}>
-
-          <Map defaultCenter={{ lat: 42.633, lng: -115.736 }} defaultZoom={6} mapId="DEMO_MAP_ID" mapTypeControl={false} streetViewControl={false}>
-            {notionData.outdoorsData.map((activityData, index) => {
-              return (<AdvancedMarker key={index} position={{ lat: activityData.coordinates.lat, lng: activityData.coordinates.lng }}>
-                <Image
-                  src={getActivityImgSrc(activityData)}
-                  width={36}
-                  height={36}
-                  alt={activityData.type}
-                />
-              </AdvancedMarker>)
-            })}
-            {notionData.travelData.map((activityData) => {
-              return activityData.coordinatesArray.map((coordinatesObj, index) => {
-                return (<AdvancedMarker key={index} position={{ lat: coordinatesObj.lat, lng: coordinatesObj.lng }}>
-                  <Image
-                    src={getActivityImgSrc(activityData)}
-                    width={36}
-                    height={36}
-                    alt={activityData.type}
-                  />
-                </AdvancedMarker>)
-              })
-            })}
-          </Map>
-
+          <CustomMap />
         </APIProvider>
       </div>
     </div>
