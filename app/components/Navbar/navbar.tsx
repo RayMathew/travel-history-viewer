1c3626888ed704e2ff3e6ce56a4cbb58b40b0ba6
Ray Mathew <ray10mathew@gmail.com>
2024-12-21 16:41:37 -0800

    make navbar responsive

--- app/components/Navbar/navbar.tsx
+++ app/components/Navbar/navbar.tsx
@@ -1,22 +1,30 @@
 import Image from "next/image";
 import NavbarAvatar from "./navbaravatar";
+import { Button } from 'primereact/button';
 
 export default function Navbar() {
 
     return (
         <>
-            <div className="flex-1 flex">
+            <div className="flex w-1/2 justify-between md:justify-start">
+                <Button
+                    className="md:hidden"
+                    icon="pi pi-bars"
+                    rounded
+                    text
+                    raised
+                />
                 <Image
-                    className="mx-4 self-center object-cover object-center rounded-lg"
+                    className="md:mx-4 self-center translate-x-1/2 md:translate-x-0 object-cover object-center rounded-lg"
                     src="/favicon.ico"
                     width={40}
                     height={40}
                     alt="Logo"
                     priority={false}
                 />
-                <div className="font-semibold self-center">Memoir Map</div>
+                <div className="font-semibold self-center hidden md:block md:translate-x-0">Memoir Map</div>
             </div>
-            <div className="p-4">
+            <div className="p-4 flex w-1/2 justify-end">
                 <NavbarAvatar />
             </div>
         </>
